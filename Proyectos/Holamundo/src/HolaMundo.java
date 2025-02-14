44b86d23d079873c1d70a536782af0f282e5661c
CRBS-10 <rbscamilo@gmail.com>
2025-02-13 21:55:36 -0600

    Se comenzaron las primeras clases :memo:

---
+++ Proyectos/Holamundo/src/HolaMundo.java
@@ -0,0 +1,18 @@
+public class HolaMundo {
+    //las llaves son el contenido de codigo, delimitan all
+    // void es vacio, pero eso indica el tipo de retorno
+    public static void main(String[] args) {
+        //static es una palabra reservada
+        //parametros de entrada "input"
+        System.out.println(
+                "Hola Mundo desde Java, este year nos hara grande");
+        //out es un atributo estatico, del tipo println, println:imprimir linea
+    }
+    //public es un modificador de acceso que indica que se puede llamar de cualquier lado
+    //Ctrl + S guarda all
+
+    //ATAJOS
+
+    //main + tab
+    //psvm +tab
+}
