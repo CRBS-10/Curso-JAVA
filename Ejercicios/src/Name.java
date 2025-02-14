56ae641f6e44bcf7b53fa2393734b1b1204491cb
CRBS-10 <rbscamilo@gmail.com>
2025-02-14 07:33:45 -0600

    Se subió la carpeta de ejercicios :zap:

---
+++ Ejercicios/src/Name.java
@@ -0,0 +1,12 @@
+
+public class Name {
+    public static void main(String[] args) {
+        //System.out.println("Camilo Raul Benitez Siguenza");
+    //podemos usar palabras reservada para un mejor desarrollo
+        // "+" se usa para concatenar
+        String Name = "Camilo Raul Benitez Siguenza" ;
+        String Dui = "06583733-1";
+        System.out.println("su nombre es: " + Name);
+        System.out.println("Su numero de Dui es: "+Dui);
+    }
+}
