a1f2bf109d5aa687efd9cf99eda71068f36135f0
CRBS-10 <rbscamilo@gmail.com>
2025-02-21 04:43:31 -0600

    Continuacion de clases :zap:

---
+++ Proyectos/Holamundo/src/CaracteresBooleanos.java
@@ -0,0 +1,30 @@
+public class CaracteresBooleanos {
+
+    public static void main(String[] args) {
+
+        // se le conoce como tipo de dato logico ya que solo son dos opciones
+        // TRUE OR FALSE
+        // RECORDAR  si se coloca "boolean" de esta forma es prmitivo si la 'b' va en mayuscula es de Referencia la apalabra
+        // esto escribiendolo el true or false
+
+        boolean datoLogico = true;
+        System.out.println("datoLogico = " + datoLogico);
+
+        // esto a apartir de la constante
+        boolean datoLogico2 = Boolean.FALSE;// aca retorna una instancia de la clase "BOOLEAN"
+        System.out.println("datoLogico2 = " + datoLogico2);
+
+        // el boolean sirve para los flujos de control como if,else, for , while, etc
+
+        double d = 98765.43e-3; // se corre a la izq dando como resultado 98.76543
+        System.out.println("d = " + d);
+        float f = 1.2345e2f; // 123.45
+        System.out.println("f = " + f);
+
+        datoLogico = d > f;
+        System.out.println("datoLogico = " + datoLogico);
+
+        boolean esIgual = 3-2 == 1 ; // un '=' solo es de asignacion con dos '==' es de comparacion
+        System.out.println("esIgual = " + esIgual);
+    }
+}
